731ad1dc62edb67c7e36a2d80fc885cb0f477b89
Supriya Jilugu <45100027+supriya8121@users.noreply.github.com>
2026-05-26 12:39:17 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://www.google.com")
+
+print("Website Title:", driver.title)
+
+driver.quit()
